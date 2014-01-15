ab868a252c8484c35173b678de089195bb497e55
Shaorune <ykmran@gmail.com>
2014-01-15 23:13:22 +0800

    Successfully created two boxes

--- main.py
+++ main.py
@@ -6,74 +6,32 @@ import wx
 
 class MyDialog(wx.Dialog):
     def __init__(self, parent, id, title):
-        wx.Dialog.__init__(self, parent, id, title, size=(600,500), style=wx.DEFAULT_DIALOG_STYLE)
+        wx.Dialog.__init__(self, parent, id, title, size=(700,500), style=wx.DEFAULT_DIALOG_STYLE)
 
         hbox  = wx.BoxSizer(wx.HORIZONTAL)
 
-        vbox1 = wx.BoxSizer(wx.VERTICAL)
-        vbox2 = wx.BoxSizer(wx.VERTICAL)
-        vbox3 = wx.GridSizer(2,2,0,0)
-        vbox4 = wx.BoxSizer(wx.VERTICAL)
+        pluginsBox = wx.BoxSizer(wx.VERTICAL)
+        versionsBox = wx.BoxSizer(wx.VERTICAL)
         
-        pnl1 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
-        pnl2 = wx.Panel(self, -1, style=wx.SIMPLE_BORDER)
+        self.plugins = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
+        self.plugins.InsertColumn(0, 'Plugin')
+        self.plugins.InsertColumn(1, 'Version')
+        self.plugins.SetColumnWidth(0, 140)
+        self.plugins.SetColumnWidth(1, 153)
         
-        self.lc = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
-        self.lc.InsertColumn(0, 'State')
-        self.lc.InsertColumn(1, 'Capital')
-        self.lc.SetColumnWidth(0, 140)
-        self.lc.SetColumnWidth(1, 153)
+        self.versions = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
+        self.versions.InsertColumn(0, 'Version')
+        self.versions.InsertColumn(1, 'Game version')
+        self.versions.SetColumnWidth(0, 140)
+        self.versions.SetColumnWidth(1, 153)
         
-        vbox1.Add(pnl1, 1, wx.EXPAND | wx.ALL, 3)
-        vbox1.Add(pnl2, 1, wx.EXPAND | wx.ALL, 3)
+        pluginsBox.Add(self.plugins, 1, wx.EXPAND | wx.ALL, 3)
+        versionsBox.Add(self.versions, 1, wx.EXPAND | wx.ALL, 3)
         
-        vbox2.Add(self.lc, 1, wx.EXPAND | wx.ALL, 3)
-        
-        self.tc1 = wx.TextCtrl(pnl1, -1)
-        self.tc2 = wx.TextCtrl(pnl1, -1)
-        
-        vbox3.AddMany([ (wx.StaticText(pnl1, -1, 'State'),0, wx.ALIGN_CENTER),
-                        (self.tc1, 0, wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL),
-                        (wx.StaticText(pnl1, -1, 'Capital'),0, wx.ALIGN_CENTER_HORIZONTAL),
-                        (self.tc2,0)])
-                        
-        pnl1.SetSizer(vbox3)
-        
-        vbox4.Add(wx.Button(pnl2, 10, 'Add'),   0, wx.ALIGN_CENTER| wx.TOP, 45)
-        vbox4.Add(wx.Button(pnl2, 11, 'Remove'), 0, wx.ALIGN_CENTER|wx.TOP, 15)
-        vbox4.Add(wx.Button(pnl2, 12, 'Clear'), 0, wx.ALIGN_CENTER| wx.TOP, 15)
-        vbox4.Add(wx.Button(pnl2, 13, 'Close'), 0, wx.ALIGN_CENTER| wx.TOP, 15)
-        
-        pnl2.SetSizer(vbox4)
-        
-        self.Bind (wx.EVT_BUTTON, self.OnAdd, id=10)
-        self.Bind (wx.EVT_BUTTON, self.OnRemove, id=11)
-        self.Bind (wx.EVT_BUTTON, self.OnClear, id=12)
-        self.Bind (wx.EVT_BUTTON, self.OnClose, id=13)
-        
-        hbox.Add(vbox1, 1, wx.EXPAND)
-        hbox.Add(vbox2, 1, wx.EXPAND)
+        hbox.Add(pluginsBox, 1, wx.EXPAND)
+        hbox.Add(versionsBox, 1, wx.EXPAND)
         
         self.SetSizer(hbox)
-
-    def OnAdd(self, event):
-        if not self.tc1.GetValue() or not self.tc2.GetValue():
-            return
-        num_items = self.lc.GetItemCount()
-        self.lc.InsertItem(num_items, self.tc1.GetValue())
-        self.lc.SetItem(num_items, 1, self.tc2.GetValue())
-        self.tc1.Clear()
-        self.tc2.Clear()
-
-    def OnRemove(self, event):
-        index = self.lc.GetFocusedItem()
-        self.lc.DeleteItem(index)
-
-    def OnClose(self, event):
-        self.Close()
-
-    def OnClear(self, event):
-        self.lc.DeleteAllItems()
 
 class Main(wx.App):
     def OnInit(self):
