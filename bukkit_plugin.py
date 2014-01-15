987fb9dba3c714dfd008d65d59bfb5319e08717a
Shaorune <ykmran@gmail.com>
2014-01-15 19:25:12 +0800

    Finished function.
    The script can now get a plugin's version download url.

--- bukkit_plugin.py
+++ bukkit_plugin.py
@@ -9,11 +9,11 @@ class bukkitPlugin(ZipFile):
         self.zip = ZipFile(path)
         with self.zip.open("plugin.yml") as pluginInfo:
             self.yaml = pluginInfo.read().decode()
-        self.version = findall("version: (.*)\r", self.yaml)
-        self.name = findall("name: (.*)\r", self.yaml)
-    
+        self.version = sub("\r|\n", "", findall("version: (.*)", self.yaml)[0])
+        self.name = sub("\r|\n", "", findall("name: (.*)", self.yaml)[0])
+
     def getGoogleResult(self):
-        url = "http://www.google.com.hk/search?q={0}+files+site%3Adev.bukkit.org%2Fbukkit-plugins".format(self.name)
+        url = "http://www.google.com.hk/search?q={0}+files+site%3Adev.bukkit.org%2Fbukkit-plugins".format(sub(" ", "+", self.name))
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         resp = opener.open(url)
@@ -21,7 +21,48 @@ class bukkitPlugin(ZipFile):
         
     def getFilesPage(self):
         self.googleResult = self.getGoogleResult()
-        print(pq(self.googleResult)(".kv").find("cite"))
+        url = "http://" + sub("<[^>]*>", "", pq(self.googleResult)(".kv").find("cite").html())
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        resp = opener.open(url)
+        return resp.read().decode()
+        
+    def getAllVersions(self):
+        self.filesPage = self.getFilesPage()
+        table = []
+        for index, column in enumerate(pq(self.filesPage)("tbody").find("tr")):
+            table.append({})
+            for cell in pq(column).find("td"):
+                if pq(cell).attr("class") == "col-file":
+                    table[index]["Name"] = {"Name": pq(cell)("a").html(), "href": "http://dev.bukkit.org" + pq(cell)("a").attr("href")}
+                elif pq(cell).attr("class") == "col-type":
+                    table[index]["Release type"] = pq(cell)("span").html()
+                elif pq(cell).attr("class") == "col-status":
+                    table[index]["Status"] = pq(cell)("span").html()
+                elif pq(cell).attr("class") == "col-date":
+                    table[index]["Date"] = int(pq(cell)("span").attr("data-epoch"))
+                elif pq(cell).attr("class") == "col-game-version":
+                    try:
+                        table[index]["Game version"] = [findall("(\d\.\d\.\d)", pq(li).html())[0] for li in pq(cell).find("li")]
+                    except:
+                        table[index]["Game version"] = [pq(li).html() for li in pq(cell).find("li")]
+                elif pq(cell).attr("class") == "col-filename":
+                    table[index]["Filename"] = sub(" +|\r|\n", "", pq(cell).html())
+                elif pq(cell).attr("class") == "col-downloads":
+                    table[index]["Downloads"] = int(pq(cell)("span").attr("data-value"))
+        return table
+        
+    def getVersionUrl(self, index):
+        try:
+            self.versions
+        except:
+            self.versions = plugin.getAllVersions()
+        downloadPageUrl = self.versions[index]["Name"]["href"]
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        resp = opener.open(downloadPageUrl)
+        downloadPage = resp.read().decode()
+        return pq(downloadPage)(".user-action-download").find("a").attr("href")
 
 plugin = bukkitPlugin(argv[1])
-plugin.getFilesPage()
+print(plugin.getVersionUrl(0))
